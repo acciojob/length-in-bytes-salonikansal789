f58951519c31e7eb40683f917a98a5982675a888
salonikansal789 <94972204+salonikansal789@users.noreply.github.com>
2024-12-05 23:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	if(str.length==0) return 0;
+	let size=str.length;
+	return size;
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
